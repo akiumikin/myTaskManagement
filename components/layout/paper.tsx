254f3a5cfab755a86724806235a0f83b3d4404c7
akiumikin <akiumikin@gmail.com>
2021-01-17 00:57:46 +0900

    aboutページのコンテンツ追加

---
+++ components/layout/paper.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import * as PaperBase from '@material-ui/core/Paper';
+import Hidden from '@material-ui/core/Hidden';
+
+interface Props {
+  children: React.ReactNode
+}
+
+export default function Paper(props: Props) {
+  return(
+    <>
+      {/* PC表示 */}
+      <Hidden smDown>
+        <PaperBase.default style={{padding: 20, margin: '30px auto 30px auto', width: 800}}>
+          {props.children}
+        </PaperBase.default>
+      </Hidden>
+
+      {/* タブレット */}
+      <Hidden mdUp xsDown>
+        <PaperBase.default style={{padding: 20, margin: '30px auto 30px auto', width: 600, maxWidth: '90%'}}>
+          {props.children}
+        </PaperBase.default>
+      </Hidden>
+
+      {/* 携帯 */}
+      <Hidden smUp>
+        <PaperBase.default style={{padding: 15, margin: '20px auto 20px auto', width: 600, maxWidth: '90%'}}>
+          {props.children}
+        </PaperBase.default>
+      </Hidden>
+    </>
+  )
+}
