d577e4d65e74104424ed512919371142f34df2e6
akiumikin <akiumikin@gmail.com>
2021-01-24 23:17:10 +0900

    textComponentのデモコードの解説からファイ名を除去２

--- components/code/form/textFieldComponentOnChangeOnBlur.tsx
+++ components/code/form/textFieldComponentOnChangeOnBlur.tsx
@@ -39,7 +39,6 @@ export default function TextFieldComponentOnChangeOnBlurCode() {
     <AvoidSSRCodeBase
       code={codeText}
       language='tsx'
-      fileName='core/form/textForm.tsx'
     />
   )
 }
